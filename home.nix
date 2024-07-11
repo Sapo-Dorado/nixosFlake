d52d1b0014b1e1810c889a267177df50b398f8b0
Nicholas Brown <nobrown@sbcglobal.net>
2024-07-11 15:39:32 -0700

    don't require ripgrep and fd for neovim

--- home.nix
+++ home.nix
@@ -18,7 +18,7 @@
 
     # The home.packages option allows you to install Nix packages into your
     # environment.
-    packages = [ neovim.packages.${pkgs.system}.default pkgs.ripgrep pkgs.fd ];
+    packages = [ neovim.packages.${pkgs.system}.default ];
 
     # Home Manager is pretty good at managing dotfiles. The primary way to manage
     # plain files is through 'home.file'.
