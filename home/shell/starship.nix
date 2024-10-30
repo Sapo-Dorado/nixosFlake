da4d596b077eee1535ae66c117895111dbfc5c1f
Nicholas Brown <nobrown@sbcglobal.net>
2024-10-29 22:30:36 -0700

    add python venv to starship

--- home/shell/starship.nix
+++ home/shell/starship.nix
@@ -3,9 +3,10 @@
     enable = true;
     settings = {
       format = ''
-        $username$hostname$directory$git_branch$git_commit$git_state$git_status$nix_shell$direnv$env_var$line_break$character
+        $username$hostname$directory$git_branch$git_commit$git_state$git_status$nix_shell$direnv$python$env_var$line_break$character
       '';
       directory = { truncation_length = 5; };
+      python = { format = "[(via \\($virtualenv\\))]($style)"; };
 
     };
   };
