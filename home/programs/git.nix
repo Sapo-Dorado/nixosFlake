7826719de6982a2bd5c5c72ff2a8aaac28544a49
Nicholas Brown <nobrown@sbcglobal.net>
2024-10-31 15:03:48 -0700

    updated global git ignores

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -4,7 +4,7 @@
     enable = true;
     userName = "Nicholas Brown";
     userEmail = "nobrown@sbcglobal.net";
-    ignores = [ ".DS_Store" "*.pyc" "*.swp" ];
+    ignores = [ ".DS_Store" "*.pyc" "*.swp" ".env" ".venv" ];
     extraConfig = {
       url."ssh://git@github.com/".insteadOf = "https://github.com/";
     };
