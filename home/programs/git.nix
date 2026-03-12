55fe571ab91de4377fa74bf4fa1b0800ba8b2920
Nicholas Brown <nobrown@sbcglobal.net>
2026-03-12 11:21:50 -0700

    update git email

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -3,7 +3,7 @@
   programs.git = {
     enable = true;
     userName = "Nicholas Brown";
-    userEmail = "nobrown@sbcglobal.net";
+    userEmail = "sapodorado@proton.me";
     ignores = [ ".DS_Store" "*.pyc" "*.swp" ".env" ".venv" ];
     extraConfig = {
       url."ssh://git@github.com/".insteadOf = "https://github.com/";
