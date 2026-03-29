9c11aeab111e36cfed3136fc923a294e4ba451c9
Nicholas Brown <countdooku.steak@gmail.com>
2026-03-29 11:20:53 -0400

    add notifiers

--- home/programs/common.nix
+++ home/programs/common.nix
@@ -1,4 +1,6 @@
 { pkgs, ... }: {
 
-  home.packages = with pkgs; [ fd ripgrep fzf just ];
+  home.packages = with pkgs;
+    [ fd ripgrep fzf just ]
+    ++ (if pkgs.stdenv.isDarwin then [ terminal-notifier ] else [ libnotify ]);
 }
