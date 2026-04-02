6be65e8adccb0c85d773b849520c6c7b7e3604ac
Nicholas Brown <sapodorado@proton.me>
2026-04-01 22:18:18 -0700

    add chrome, update nix pkgs

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "Personal NixOS/Home-Manager Configuration";
 
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -2,7 +2,7 @@
   imports = [ ./docker.nix ./packages.nix ./timezone.nix ];
 
   nix = {
-    package = pkgs.nixFlakes;
+    package = pkgs.nixVersions.stable;
     extraOptions = "experimental-features = nix-command flakes";
   };
 
